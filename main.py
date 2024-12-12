0e8db6e998b556b3ded7792c3c9b3eb1e972ea5c
carlos.fernandez <carlosfernandez.nieto@escuelaestech.es>
2024-12-12 09:45:16 +0100

    Terminada parte 1

---
+++ main.py
@@ -0,0 +1,9 @@
+a = 5
+b = 3
+
+if a > b:
+    print(f"La variable 'a' ({a}) es mayor que la variable 'b' ({b}).")
+elif b > a:
+    print(f"La variable 'b' ({b}) es mayor que la variable 'a' ({a}).")
+else:
+    print(f"Ambas variables son iguales: a = {a} y b = {b}.")
